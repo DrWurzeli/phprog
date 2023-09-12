272e9c90ad2a1e875f211de54a7c63eecdecb3bf
Maximilian Sander <github@maxmailbox.de>
2023-09-12 15:15:36 +0200

    task 03.02

---
+++ 03_Einfuehrung/aufgabe03.02.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8"/>
+        <title>Aufgabe 3.2</title>
+    </head>
+    <body>
+        <h1>Headline 1</h1>
+        <p>Paragraph</p>
+        <ul>
+            <li>1</li>
+            <li>2</li>
+            <li><a href="https://php.net">3</a></li>
+        </ul>
+        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/0e/Hochschule_f%C3%BCr_Oekonomie_%26_Management_2012_logo.svg/480px-Hochschule_f%C3%BCr_Oekonomie_%26_Management_2012_logo.svg.png">
+    </body>
+</html>
