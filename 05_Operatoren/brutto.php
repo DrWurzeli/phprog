06fb1d6b86892eca7864cb7f92dc495044f85529
Maximilian Sander <github@maxmailbox.de>
2023-09-19 13:46:33 +0200

    task 05.01

---
+++ 05_Operatoren/brutto.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8"/>
+        <title>Aufgabe 5.1</title>
+    </head>
+    <body>
+        <?php
+            $preise = array(22.5, 12.30, 5.20);
+            $nettosumme = 0;
+            $ust = 1.19;
+            $bruttosumme = 0;
+            for($i=0; $i<count($preise); $i++){
+                $nettosumme += $preise[$i];
+                echo "Artikel ".($i+1).": ".$preise[$i]." Euro"."<br>";
+                $bruttosumme += $preise[$i]*$ust;
+            }
+            echo "<br>";
+            echo "Nettosumme: ".$nettosumme."Euro <br>";
+            echo "Umsatzsteuer: 19 %<br>";
+            echo "Bruttosumme: ".$bruttosumme."Euro <br";
+        ?>
+    </body>
+</html>
