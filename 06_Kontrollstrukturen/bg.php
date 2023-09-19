6ddeca7735a4287316fa7572c146f86260c9569e
Maximilian Sander <github@maxmailbox.de>
2023-09-19 14:23:52 +0200

    task 06.01

---
+++ 06_Kontrollstrukturen/bg.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8"/>
+    <title>changing bg</title>
+    <style>
+        body {
+            background-color:
+            <?php
+            date_default_timezone_set("Europe/Berlin");
+            $time = date("H");
+            if ($time < 5  || $time > 20) echo "black";
+            elseif ($time < 11) echo "lightblue";
+            elseif ($time <15) echo "blue";
+            elseif ($time <18) echo "yellow";
+            else echo "orange";
+            ?>;
+        }
+    </style>
+</head>    
+<body>
+    <?php
+        echo "hallo";
+    ?>
+</body>
+</html>
