351678d542b7375fa0512339b7a9100efd0fa0fa
Maximilian Sander <github@maxmailbox.de>
2023-09-26 12:55:06 +0200

    missing dot

--- 07_Funktionen/aufgabe07.01.php
+++ 07_Funktionen/aufgabe07.01.php
@@ -3,7 +3,7 @@
 function vermerk($vorname, $nachname, $abteilung) {
     echo "Programm von ".$vorname." ".$nachname." Abteilung: ".$abteilung;
     echo "<br>";
-    echo "E-Mail: ".strtolower($vorname).".".strtolower($nachname)."@".strtolower($abteilung)."phpdeve1.de";
+    echo "E-Mail: ".strtolower($vorname).".".strtolower($nachname)."@".strtolower($abteilung).".phpdeve1.de";
 }
 
 vermerk("Bodo", "Berg", "FE2");
