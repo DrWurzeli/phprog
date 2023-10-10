3b8e47b79adc526b582adbaca138fbd0bd43a0b3
Maximilian Sander <github@maxmailbox.de>
2023-10-10 13:34:36 +0200

    task09.02 incl. files

---
+++ 09_Files/aufgabe09.02.php
@@ -0,0 +1,40 @@
+<?php
+    //check for file
+$people = 0;
+$over1k = 0;
+$under1k = 0;
+   if(file_exists("u_schreiben.txt")) {
+        //open file
+        $file = @fopen("u_schreiben.txt", "r");
+        //did file open?
+        if ($file){
+            //is file empty?
+            while (!feof($file)) {
+                //get identifier
+                $people++;
+                $id = fgets($file);
+                if ($id >= 1000){
+                    $over1k++;
+                    $fileb = fopen("u_schreiben_b", "w");
+                    if($fileb){
+                        fputs($fileb, fgets($file)."\n".fgets($file));
+                        fclose($fileb);
+                    }
+                }
+                else{
+                    $under1k++;
+                    $filea = fopen("u_schreiben_a", "w");
+                    if($filea){
+                        fputs($filea, fgets($file)."\n".fgets($file));
+                        fclose($filea);
+                    }
+                }
+            }
+            echo "Es wurden $people Datensaetze gefunden.<br>";
+            echo "Davon wurden $under1k in die Datei u_schreiben_a.txt geschrieben<br>";
+            echo "und $over1k in die Datei u_schreiben_a geschrieben.<br>";
+            fclose($file);
+        }
+        else "Datei konnte nicht geöffnet werden.";
+   }
+   else echo "Keine Datei gefunden.";
