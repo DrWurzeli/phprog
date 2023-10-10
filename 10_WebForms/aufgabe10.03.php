ad1aa0a5d597b98bd86178ab11a3f0a9cb6532fb
BuildTools <github@maxmailbox.de>
2023-10-10 21:05:51 +0200

    task10.03 with checks

--- 10_WebForms/aufgabe10.03.php
+++ 10_WebForms/aufgabe10.03.php
@@ -3,40 +3,33 @@
     <title>Forms</title>
 </head>
 <body>
-<form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]) ?>" method="POST">
-Nettowert: <br />
-<input type="text" name="netto" size="20" maxlength="30" />
-<br />
-USt-Satz:<br>
-<select name="ust">
-    <option value="19">19%</option>
-    <option value="7">7%</option>
-</select>
-<br>
-<input type="submit" value="Berechnen" />
-</form>
+    <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"])?>" method="POST">
+    Nettowert:<br>
+    <input type="text" name="netto" size="20" maxlength="30"/>
+    <br>
+    USt-Satz:<br>
+    <select name="ust">
+        <option value="19">19%</option>
+        <option value="7">7%</option>
+    </select>
+    <br>
+    <input type="submit" value="Berechnen"/>
+    </form>
 </body>
 </html>
 
 <?php
-$betrag = -1;
-$ust = -1;
-$brutto = 0;
+    function brutto($netto, $mwstSatz){
+        return $netto * (100 + $mwstSatz) / 100;
+    }
 
-function brutto($netto, $mwstSatz){
-    return $netto * (100 + $mwstSatz) / 100;
-}
-if(isset($POST_['netto']) && !empty($POST_['netto'])){
-    $betrag = $POST_['netto'];
-    echo $betrag;
-}
-
-if(isset($POST_['ust']) && !empty($POST_['ust'])){
-    $ust = $POST_['ust'];
-    echo $ust;
-}
-
-if ($betrag <> -1 && $ust <> -1){
-    $brutto = brutto($betrag, $ust);
-    echo "$betrag ergibt $brutto inklusive USt.<br>";
-}
+    if(isset($_POST['netto']) && isset($_POST['ust']) && $_SERVER['REQUEST_METHOD'] == 'POST'){
+        if (empty($_POST['ust']) || empty($_POST['netto'])){
+            echo "<p>Bitte etwas in die Felder eintragen.<br>";
+        }
+        else{
+            $brutto = brutto($_POST['netto'], $_POST['ust']);
+            echo htmlspecialchars($_POST['netto'])." ergibt ".htmlspecialchars($brutto)." inklusive USt.<br>"; 
+        }
+    }
+?>
