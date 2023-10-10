526661d7facf8029b60b3f2c97c7f8a48ada56a2
Maximilian Sander <github@maxmailbox.de>
2023-10-10 13:42:47 +0200

    added file checks to beginning to avoid premature closing

--- 09_Files/aufgabe09.02.php
+++ 09_Files/aufgabe09.02.php
@@ -4,10 +4,12 @@ $people = 0;
 $over1k = 0;
 $under1k = 0;
    if(file_exists("u_schreiben.txt")) {
-        //open file
-        $file = @fopen("u_schreiben.txt", "r");
+        //open files
+        $file = fopen("u_schreiben.txt", "r");
+        $fileb = fopen("u_schreiben_b.txt", "w");
+        $filea = fopen("u_schreiben_a.txt", "w");
         //did file open?
-        if ($file){
+        if ($file && $filea && $fileb){
             //is file empty?
             while (!feof($file)) {
                 //get identifier
@@ -15,25 +17,19 @@ $under1k = 0;
                 $id = fgets($file);
                 if ($id >= 1000){
                     $over1k++;
-                    $fileb = fopen("u_schreiben_b.txt", "w");
-                    if($fileb){
-                        fputs($fileb, fgets($file).fgets($file));
-                        fclose($fileb);
-                    }
+                    fputs($fileb, $id.fgets($file).fgets($file)); 
                 }
                 else{
                     $under1k++;
-                    $filea = fopen("u_schreiben_a.txt", "w");
-                    if($filea){
-                        fputs($filea, fgets($file).fgets($file));
-                        fclose($filea);
-                    }
+                    fputs($filea, $id.fgets($file).fgets($file));
                 }
             }
             echo "Es wurden $people Datensaetze gefunden.<br>";
             echo "Davon wurden $under1k in die Datei u_schreiben_a.txt geschrieben<br>";
             echo "und $over1k in die Datei u_schreiben_a geschrieben.<br>";
             fclose($file);
+            fclose($filea);
+            fclose($fileb);
         }
         else "Datei konnte nicht geöffnet werden.";
    }
