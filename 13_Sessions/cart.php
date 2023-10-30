12a268f729f39dca00e8401e046042e23d3380c4
Maximilian Sander <github@maxmailbox.de>
2023-10-30 11:50:05 +0100

    shop task

--- 13_Sessions/cart.php
+++ 13_Sessions/cart.php
@@ -2,24 +2,27 @@
 session_start();
 
 if(isset($_POST["amount"])){
-    $_SESSION["article"] = "test";
+    var_dump($_POST["amount"]);
 }
 
-if($isset($_SESSION["article"])){
+if(isset($_SESSION["cart"]) && !empty($_SESSION["cart"])){
     ?>
         <form action="<?php echo "checkout.php" ?>" method="POST">
     <?php
         echo "<table border='1'><th>Name</th><th>Nummer</th><th>Preis</th><th>Anzahl</th>";
-        echo "<tr>";
-        echo "</tr>";
-        echo "</table>";
+        for($x=0; $x<count($_SESSION["cart"]); $x++){
+            echo "<tr>";
+            echo "<td>".$_SESSION["cart"][$x][0]."</td>";
+            echo "</tr>";
+            echo "</table>";
+        }
     ?>
         <input type="submit" value="Zur Kasse" name="checkout" />
         </form>
     <?php
 }
 else{
-    echo "Sie haben noch keine Artikel hinzugefügt.<br>";
+    echo "<br>Sie haben noch keine Artikel hinzugefügt.<br>";
 }
 
 
