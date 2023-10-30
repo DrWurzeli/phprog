f26632ebf22fe8d00cc2c8fa25e59ca6141d888e
Maximilian Sander <github@maxmailbox.de>
2023-10-30 09:45:50 +0100

    small check

--- 12_Cookies/aufgabe12.02a.php
+++ 12_Cookies/aufgabe12.02a.php
@@ -6,10 +6,20 @@
 <body>
 <form action="aufgabe12.02b.php" method="POST">
 <h2>Ihr Name</h2>
-<input type="text" name="nachname" size="20" maxlength="30" value="<?php echo htmlspecialchars($_COOKIE["nachname"]); ?>" />
+<input type="text" name="nachname" size="20" maxlength="30" value="
+<?php
+if (isset($_COOKIE["nachname"])){
+    echo htmlspecialchars($_COOKIE["nachname"]);
+ }
+?>" />
 Nachname
 <br><br>
-<input type="text" name="vorname" size="20" maxlength="30" value="<?php echo htmlspecialchars($_COOKIE["vorname"]); ?>"/>
+<input type="text" name="vorname" size="20" maxlength="30" value="
+<?php
+if (isset($_COOKIE["vorname"])){
+    echo htmlspecialchars($_COOKIE["vorname"]);
+ }
+?>"/>
 Vorname
 <br><br>
 <input type="submit" value="Bestellen" />
